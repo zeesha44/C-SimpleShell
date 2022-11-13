320ad9655da74657b4c394396ac23897d65e0c86
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-11-13 11:56:22 +0100

    bgvn

--- getenv.c
+++ getenv.c
@@ -2,7 +2,7 @@
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments. 
  * Return: Always 0
  */
 char **get_environ(info_t *info)
@@ -17,7 +17,7 @@ char **get_environ(info_t *info)
 
 /**
  * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments.
  * Return: 1 on delete, 0 otherwise
  * @var: the string env var property
  */
@@ -47,7 +47,7 @@ int _unsetenv(info_t *info, char *var)
 }
 /**
  * _setenv - Initialize a new environment variable, or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @info: Structure containing potential arguments.
  * @var: the string env var property
  * @value: the string env var value
  * Return: Always 0
