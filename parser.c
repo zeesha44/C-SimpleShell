b8c5c88a9f55ade85442a4a493e934e231c57946
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-11-17 00:12:15 +0100

    vbnm

--- parser.c
+++ parser.c
@@ -33,10 +33,9 @@ int is_cmd(info_t *info, char *path)
 char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
-	int i = 0;
-	int k = 0;
+	int i = 0, k = 0;
 
-	for (k = 0; i = start; i < stop; i++)
+	for (k = 0, i = start; i < stop; i++)
 		if (pathstr[i] != ':')
 			buf[k++] = pathstr[i];
 	buf[k] = 0;
