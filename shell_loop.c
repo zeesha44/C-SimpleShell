5ceea76e8972d7d8d93b54b980745a1b90cf23bf
zainab shaheedah <56594700+zeesha44@users.noreply.github.com>
2022-11-16 23:01:12 +0100

    bnm

--- shell_loop.c
+++ shell_loop.c
@@ -104,7 +104,7 @@ void find_cmd(info_t *info)
 	}
 	else
 	{
-		if ((interactive(info) || _getenv(info, "PATH=") 
+		if ((interactive(info) || _getenv(info, "PATH=")
 			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
 			fork_cmd(info);
 		else if (*(info->arg) != '\n')
