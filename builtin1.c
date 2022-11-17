d09eee7d03a4ad58dea2ca124e182f1103d72f92
Baraka Aminu <56594657+Baraka1234@users.noreply.github.com>
2022-11-17 17:19:12 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -27,7 +27,8 @@ int unset_alias(info_t *info, char *str)
 		return (1);
 	c = *p;
 	*p = 0;
-	ret = delete_node_at_index(&(info->alias), get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	ret = delete_node_at_index(&(info->alias), 
+				   get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 	*p = c;
 	return (ret);
 }
